d82d5c01441b90d4df3c86377988924239aae52e
Muhiddin Kurbonov <muhiddindc@gmail.com>
2025-09-23 20:16:58 -0500

    add DbContext

---
+++ PROJECTS/Project-1/BugTrakr/src/Data/DbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using BugTrakr.Models;
+
+namespace BugTrakr.Data;
+
+public class BugTrakrDbContext : DbContext
+{
+    public BugTrakrDbContext(DbContextOptions<BugTrakrDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
