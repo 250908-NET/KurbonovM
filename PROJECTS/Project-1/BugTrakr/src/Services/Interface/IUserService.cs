d371fa7d00f4cc491c10ccf26237c4c21c5133b5
Muhiddin Kurbonov <muhiddindc@gmail.com>
2025-09-23 22:55:32 -0500

    add IUserService interface

---
+++ PROJECTS/Project-1/BugTrakr/src/Services/Interface/IUserService.cs
@@ -0,0 +1,14 @@
+using BugTrakr.Models;
+
+namespace BugTrakr.Services
+{
+    public interface IUserService
+    {
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<List<User>> GetAllUsersAsync();
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+    }
+}
